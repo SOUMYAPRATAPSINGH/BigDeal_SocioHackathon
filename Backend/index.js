1fd094bf7ee5532fa01a1ff49404aae62772b1b0
raoyashwant <raoyashwant132@gmail.com>
2024-01-20 14:59:01 +0530

    Backend

---
+++ Backend/index.js
@@ -0,0 +1,51 @@
+// EXPRESS CONNECTION
+
+const express = require('express')
+const app = express()
+const port = 8000
+app.listen(port,(err)=>{
+    if (err) throw err;
+    console.log(`Server is running at http://localhost:${port}`) 
+})
+
+// DATABASE CONNECTION
+
+const mongoose = require('mongoose')
+const User = require('./schema')
+
+const URL = `mongodb+srv://raoyashwant132:Xchange@mobile.qd2x1vb.mongodb.net/Xchange?retryWrites=true&w=majority`
+mongoose.connect(URL)
+.then(()=>{
+    console.log("Connected to database")
+})
+.catch((err)=>{
+    console.log(err)
+})
+
+
+// MIDDLEWEARS
+
+app.use(express.urlencoded({extended: true}));
+app.use(express.json());
+
+// API
+
+app.post('/login',async(req,res)=>{
+    console.log("Login Details - ",req.body)
+    const result = await User.findOne(req.body)
+    result?res.json({status: 200,msg: 'User Found in Database'}):res.json({status: 400,msg: 'Wrong Credentials'})
+})
+
+app.post('/signup',async(req,res)=>{
+    console.log("Signup Details - ",req.body)
+    const user = new User(req.body)
+    const result = await user.save()
+    if(result){
+        console.log("User Created")
+        res.status(200).json({msg: 'details reached for signup'})
+    }
+    else
+    {
+        console.log("Error in creating user")
+    }
+})
